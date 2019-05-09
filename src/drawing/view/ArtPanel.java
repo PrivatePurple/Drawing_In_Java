323da8b1dc81a23f93f82a0474c92042d9658a13
Keenan <Keenanking555@gmail.com>
2019-05-09 13:17:47 -0600

    Added constructors!

--- src/drawing/view/ArtPanel.java
+++ src/drawing/view/ArtPanel.java
@@ -1,16 +1,70 @@
 package drawing.view;
 
-import javax.swing.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import drawing.controller.ArtController;
 
 public class ArtPanel extends JPanel
 {
 	private ArtController app;
+	private Color currentColor;
+	private BufferedImage currentCanvas;
+	private int previousX;
+	private int previousY;
 	
 	public ArtPanel(ArtController app)
 	{
 		super();
-		
 		this.app = app;
+		this.currentCanvas = new BufferedImage(700, 700, BufferedImage.TYPE_INT_ARGB);
+		setupPanel();
+		resetPoint();
 	}
+	
+	public void resetPoint()
+	{
+		previousX = Integer.MIN_VALUE;
+		previousY = Integer.MIN_VALUE;
+	}
+	
+	public void clearImage()
+	{
+		
+	}
+	
+	public void drawLine(int currentX, int currentY, int width)
+	{
+		
+	}
+	
+	public void drawDot(int currentX, int currentY, int width)
+	{
+		
+	}
+
+	public void saveImage()
+	{
+		
+	}
+	
+	public void setupPanel()
+	{
+		
+	}
+
+//Nice
 }
